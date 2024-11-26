69ed01e8c7e04c8c86b6417a05aea26a9ca1fdeb
Kristian Quirapas <kmquirapas@gmail.com>
2024-11-27 01:34:23 +0800

    feat: Error handlers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 mod cache;
+mod error;
 mod rpc;
 mod svm;
 
-fn main() {
-    println!("Hello, Sonic!");
+use anyhow::Result;
+use clap::{Arg, Command};
+
+fn main() -> Result<()> {
+    let matches = Command::new("grid-v2")
+        // .version("0.1.0") // commented out to get version from manifest
+        .about("Grid V2")
+        .arg_required_else_help(true)
+        .arg(Arg::new("NAME").help("Name").long("name").short('n'))
+        .get_matches();
+
+    Ok(())
 }
