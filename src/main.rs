da657e61abc0be69788407f76571245656e87d85
Kristian Quirapas <kmquirapas@gmail.com>
2024-11-27 13:53:31 +0800

    chore: Reorganize workspace

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use anyhow::Result;
 use clap::{Arg, Command};
 
 fn main() -> Result<()> {
-    let matches = Command::new("grid-v2")
+    let matches = Command::new("grid")
         // .version("0.1.0") // commented out to get version from manifest
         .about("Grid V2")
         .arg_required_else_help(true)
         .arg(
-            Arg::new("RPC_URL")
+            Arg::new("RPC")
                 .help("Base layer RPC URL")
                 .long("rpc-url")
                 .short('r'),
